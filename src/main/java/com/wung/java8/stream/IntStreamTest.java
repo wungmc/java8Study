db9f2596b19ce6a6958beecfc03e62140e1547ab
wung <wungcc@gmail.com>
2018-08-11 18:32:16 +0800

    重写勾股对

--- src/main/java/com/wung/java8/stream/IntStreamTest.java
+++ src/main/java/com/wung/java8/stream/IntStreamTest.java
@@ -12,7 +12,7 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
- * 原始类型专用流:IntStream, LongStream, DoubleStream.
+ * 原始类型特化流:IntStream, LongStream, DoubleStream.
  * 为了避免使用 Stream<Integer> 所带来的默认装箱成本。
  * 而且还提供了一些针对数值的规约接口，如：sum, max等。
  * 还可以在需要的时候转化为 Stream<T> 类型的流。
@@ -37,7 +37,7 @@ public class IntStreamTest {
 	);
 	
 	/**
-	 * 转化为专用流
+	 * 转化为特化流
 	 */
 	@Test
 	public void mapToInt() {
@@ -67,7 +67,7 @@ public class IntStreamTest {
 	}
 	
 	/**
-	 * 专用流转回一般流
+	 * 特化流转回一般流
 	 */
 	@Test
 	public void boxed() {
@@ -117,6 +117,50 @@ public class IntStreamTest {
 				;
 		
 		doubleStream.forEach(d -> System.out.println(d[0] + "," + d[1] + "," + d[2]));
+		
+		// out
+		// 3.0,4.0,5.0
+		// 5.0,12.0,13.0
+		// 6.0,8.0,10.0
+		// 7.0,24.0,25.0
+		// 8.0,15.0,17.0
+		// 9.0,12.0,15.0
+		// 9.0,40.0,41.0
+		// 10.0,24.0,26.0
+		// 12.0,16.0,20.0
+		// 12.0,35.0,37.0
+		// 14.0,48.0,50.0
+		// 15.0,20.0,25.0
+		// 15.0,36.0,39.0
+		// 16.0,30.0,34.0
+		// 18.0,24.0,30.0
+		// 20.0,21.0,29.0
+		// 20.0,48.0,52.0
+		// 21.0,28.0,35.0
+		// 24.0,32.0,40.0
+		// 24.0,45.0,51.0
+		// 27.0,36.0,45.0
+		// 28.0,45.0,53.0
+		// 30.0,40.0,50.0
+		// 33.0,44.0,55.0
+		// 36.0,48.0,60.0
+		// 40.0,42.0,58.0
 	}
+	
+	/**
+	 * 重写勾股对（为了熟练）
+	 */
+	@Test
+	public void pythagorean2() {
+		Stream<double[]> doubleStream = IntStream.rangeClosed(1, 50).boxed()
+				.flatMap(
+						a -> IntStream.rangeClosed(a + 1, 50)
+							.mapToObj(b -> new double[]{a, b, Math.sqrt(a*a + b*b)})
+							.filter(c -> c[2] % 1 == 0)
+				);
+		
+		doubleStream.forEach(a -> System.out.println(a[0] + "," + a[1] + "," + a[2]));
+	}
+	
 	
 }
