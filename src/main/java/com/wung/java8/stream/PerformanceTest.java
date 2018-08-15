dcc4dbc6ba5f7fc7859f8c57097937511e841db4
wung <wungcc@gmail.com>
2018-08-15 10:01:14 +0800

    性能测试

---
+++ src/main/java/com/wung/java8/stream/PerformanceTest.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (C), 2011-2018.
+ */
+package com.wung.java8.stream;
+
+import org.junit.Test;
+
+import java.util.function.Function;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+/**
+ * @author wung 2018/8/14.
+ */
+public class PerformanceTest {
+	
+	/**
+	 * 测试框架。
+	 * 因为 JIT 有预热，故多运行几次。
+	 */
+	public static long testFramework(Function<Long, Long> adder, long n) {
+		long fastest = Long.MAX_VALUE;
+		for (int i = 0; i < 10; i++) {
+			long start = System.nanoTime();
+			
+			long sum = adder.apply(n);
+			
+			long duration = (System.nanoTime() - start) / 1000000;
+			if (duration < fastest) {
+				fastest = duration;
+			}
+			System.out.println("和：" + sum);
+		}
+		return fastest;
+	}
+	
+	/**
+	 * 顺序求自然数前n的和。
+	 */
+	public static long seqSum(long n) {
+		return Stream.iterate(1L, a -> a + 1)
+				.limit(n)
+				.reduce(0L, Long::sum);
+	}
+	
+	/**
+	 * 普通迭代求自然数前n的和。
+	 */
+	public static long iterativeSum(long n) {
+		long sum = 0L;
+		for (long j = 1L; j <= n; j++) {
+			sum += j;
+		}
+		return sum;
+	}
+	
+	
+	/**
+	 * 并行流求自然数前n的和。
+	 */
+	public static long parallelSum(long n) {
+		long sum = Stream.iterate(1L, a -> a + 1)
+				.limit(n)
+				.parallel()
+				.reduce(0L, Long::sum);
+		return sum;
+	}
+	
+	
+	/**
+	 * LongStream 并行流求自然数前n的和。
+	 */
+	public static long longStreamParalleSum(long n) {
+		long sum = LongStream.rangeClosed(1L, n).parallel().reduce(0L, Long::sum);
+		return sum;
+	}
+	
+	
+	@Test
+	public void seqSumTest() {
+		System.out.println("seqSumTest耗时：" + testFramework(PerformanceTest::seqSum, 1000_0000));
+		
+		// out
+		// seqSumTest耗时：125
+	}
+	
+	@Test
+	public void iterativeSumTest() {
+		System.out.println("iterativeSumTest耗时：" + testFramework(PerformanceTest::iterativeSum, 1000_0000));
+		
+		// out
+		// iterativeSumTest耗时：4
+	}
+	
+	@Test
+	public void parallelSumTest() {
+		System.out.println("parallelSumTest耗时：" + testFramework(PerformanceTest::parallelSum, 1000_0000));
+		
+		// out
+		// parallelSumTest耗时：390
+		
+		// 可以看出这种方法——使用 iterate 方法生成的流进行并行计算——是最低效。
+		// 因为 iterate 是以函数来生成流，流的每一项都依赖前一项，流是顺序的，所以不适合分块计算，并行计算反而更低效。
+	}
+	
+	@Test
+	public void longStreamParalleSumTest() {
+		System.out.println("longStreamParalleSumTest耗时：" + testFramework(PerformanceTest::longStreamParalleSum, 1000_0000));
+		
+		// out
+		// longStreamParalleSumTest耗时：3
+	}
+	
+	
+}
